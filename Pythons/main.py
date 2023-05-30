25e37ff33bf11219c003bd416935cc99ed7cba95
Federico Paci <fmpaci88@gmail.com>
2023-05-30 17:45:59 -0400

    Test with colors

--- Pythons/main.py
+++ Pythons/main.py
@@ -3,9 +3,17 @@ from shapely.geometry import shape, mapping
 from shapely.ops import unary_union
 
 # Ruta de los archivos GeoJSON
-ruta_poligono_mayor = 'sao_paulo_general.geojson'
+ruta_poligono_mayor = 'stanford_spo.geojson'
 ruta_areas_mas_pequenas = 'spo_microzones.geojson'
 ruta_salida = 'ruta_del_archivo_salida.geojson'
+
+# Leer el archivo del polígono mayor
+with open(ruta_poligono_mayor) as file:
+    data_poligono_mayor = json.load(file)
+
+# Leer el archivo de las áreas más pequeñas
+with open(ruta_areas_mas_pequenas) as file:
+    data_areas_mas_pequenas = json.load(file)
 
 
 # Crear los objetos de geometría de Shapely para el polígono mayor y las áreas más pequeñas
@@ -15,6 +23,7 @@ areas_mas_pequenas = [shape(feature['geometry']) for feature in data_areas_mas_p
 # Restar las áreas más pequeñas del polígono mayor
 resultado = poligono_mayor.difference(unary_union(areas_mas_pequenas))
 
+print(resultado)
 # Crear un nuevo GeoJSON con el resultado combinado
 resultado_geojson = {
     "type": "FeatureCollection",
